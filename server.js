9d0cbef924221243016f60b743583d26256a9d16
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-07-22 04:02:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { Server } = require("socket.io");
+const http = require('http')
+const app = express();
+const cors = require('cors')
+
+app.use(cors())
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors: {
+        origin:"http://localhost:5173",
+        methods:["GET", "POST"]
+    }
+})
+
+//listen to events from FrontEnd;
+//when a user connects on out FR -> This immediately starts running
+io.on("connection", (socket)=>{
+    console.log(`USER CONNECTED: ${socket.id}`)
+
+    socket.on("send_message" , (data) => {
+        socket.broadcast.emit("receive_message", data)
+    })
+})
+
+app.get('/', (req,res)=> {
+    res.send("Home of Serve")
+})
+
+server.listen(5000,() => {
+    console.log("Server on Port 5000" )
+})
