ed3c8514eb3282d41f5d5686624747e11713c40a
shwetank00001 <51539873+shwetank00001@users.noreply.github.com>
2025-07-22 04:21:40 +0530

    updated URL for render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors: {
-        origin:"http://localhost:5173",
+        origin:"https://boisterous-fairy-08e652.netlify.app/",
         methods:["GET", "POST"]
     }
 })
